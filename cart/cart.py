8ce1c6537fc685b1f23926b13cde653d1a01f6b2
RachanaMaskey <rachanamaskey@gmail.com>
2024-11-27 10:15:24 +0545

    cart update,delete functionalities

--- cart/cart.py
+++ cart/cart.py
@@ -17,14 +17,16 @@ class Cart():
         self.cart=cart
 
 
-    def add(self,product):
+    def add(self,product,quantity):
         product_id=str(product.id)
+        product_qty=str(quantity)
 
         #logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id]={'price':str(product.price)}
+            # self.cart[product_id]={'price':str(product.price)}
+            self.cart[product_id]=int(product_qty)
 
         self.session.modified= True
 
@@ -38,3 +40,57 @@ class Cart():
         #use ids to lookup products in database model
         products=Product.objects.filter(id__in=product_ids)
         return products
+    
+    def cart_total(self):
+        #get product id
+        product_ids=self.cart.keys()
+
+        #lookup those keys in product database model
+        products=Product.objects.filter(id__in=product_ids)
+
+        #get quantities
+        quantities=self.cart
+
+        #start couning at 0
+        total=0
+        for key,value in quantities.items():
+            #convert key string into int
+            key=int(key)
+            for product in products:
+                if product.id==key:
+                    if product.is_sale:
+                        total=total+(product.sale_price*value)
+                    else:
+                        total=total+(product.price*value)
+        
+        return total
+
+
+    
+    def get_quants(self):
+        quantities=self.cart
+        return quantities
+    
+    def update(self,product,quantity):
+        product_id=str(product)
+        product_qty=int(quantity)
+
+        #get cart
+        ourcart=self.cart
+
+        #update dictionary/cart
+        ourcart[product_id]=product_qty
+
+        self.session.modified=True
+        thing=self.cart
+
+        return thing
+    
+    def delete(self,product):
+        product_id=str(product)
+
+        #delete from cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified=True
--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,9 @@ def cart_summary(request):
     #get the cart
     cart=Cart(request)
     cart_products=cart.get_prods
-    return render(request,"cart_summary.html",{"cart_products": cart_products})
+    quantities=cart.get_quants
+    totals= cart.cart_total()
+    return render(request,"cart_summary.html",{"cart_products": cart_products,"quantities":quantities,"totals":totals})
 
 def cart_add(request):
     #get the cart
@@ -19,12 +21,13 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         #get stuff
         product_id=int(request.POST.get('product_id'))
+        product_qty=int(request.POST.get('product_qty'))
 
         #lookup product in DB
         product=get_object_or_404(Product,id=product_id)
 
         #save into a session
-        cart.add(product=product)
+        cart.add(product=product,quantity= product_qty)
         
         #get cart quantity
         cart_quantity=cart.__len__()
@@ -38,7 +41,25 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart=Cart(request)
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id=int(request.POST.get('product_id')) 
+
+        #call delete function
+        cart.delete(product=product_id)
+        response=JsonResponse({'product':product_id})
+        return response
+
 
 def cart_update(request):
-    pass
+    cart=Cart(request)
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id=int(request.POST.get('product_id'))
+        product_qty=int(request.POST.get('product_qty'))
+
+        cart.update(product=product_id,quantity=product_qty)
+        response=JsonResponse({'qty':product_qty})
+        return response
+    
