2cc80eaa698711e6d4c041c4ffe9a6566b4a6e26
RachanaMaskey <rachanamaskey@gmail.com>
2024-12-07 11:15:22 +0545

    checkout update

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ecommerce.models import Product
+from django.db.models.signals import post_save
+
 
 class ShippingAddress(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
@@ -19,6 +21,17 @@ class ShippingAddress(models.Model):
     def __str__(self):
         # return f'Shipping Address-{str(self.id)}' this is for id 
         return f'{str(self.shipping_full_name),str(self.id)}'
+    
+
+     
+    #create user profile by default when the user signs up
+    def create_shipping(sender,instance,created, **kwargs):
+        if created:
+          user_shipping=ShippingAddress(user=instance)
+          user_shipping.save()
+
+    #automate the profile thing
+    post_save.connect(create_shipping,sender=User)
     
 #Create order model
 class Order(models.Model):
